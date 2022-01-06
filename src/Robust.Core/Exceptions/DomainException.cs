16cf1b3f17ae7c20cf0608a4301acc6487e172a0
Ricardo Rocha <jricardo@minsait.com>
2022-01-06 19:50:22 -0300

    Cleaned code.

--- src/Robust.Core/Exceptions/DomainException.cs
+++ src/Robust.Core/Exceptions/DomainException.cs
@@ -3,22 +3,22 @@
     public class DomainException : Exception
     {
         internal List<string> _errors = new List<string>();
-        
+
         public IReadOnlyCollection<string> Errors => _errors;
 
         public DomainException()
         {
         }
-        
+
         public DomainException(string message, List<string> errors) : base(message)
         {
             _errors = errors;
-        } 
-        
+        }
+
         public DomainException(string message) : base(message)
         {
         }
-        
+
         public DomainException(string message, Exception innerException) : base(message, innerException)
         {
         }
